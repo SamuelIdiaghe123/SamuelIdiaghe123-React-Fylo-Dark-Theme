c91ea5bd7c928f01983d91cb11e200573cbb40dd
Samuel Idiaghe <99045253+SamuelIdiaghe123@users.noreply.github.com>
2023-04-12 12:58:15 +0100

    Update navbar.jsx

--- src/fylo-dark-theme-landing-page-master/components/navbar.jsx
+++ src/fylo-dark-theme-landing-page-master/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap-icons/font/bootstrap-icons.css"
-import "/Users/User/Documents/Completed Side Projects/React/my-app/src/App.css";
+import "/Users/User/Documents/Completed Side Projects/React/react-app/src/App.css";
 import Logo from "../images/logo.svg"
 
 export const Navbar = () => {
