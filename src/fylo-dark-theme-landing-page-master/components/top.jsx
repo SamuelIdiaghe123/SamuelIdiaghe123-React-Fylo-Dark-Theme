cd35ddb5d2a7176a224df6a7c6d99f65fbd8dd21
Samuel Idiaghe <99045253+SamuelIdiaghe123@users.noreply.github.com>
2023-04-12 12:58:39 +0100

    Update top.jsx

--- src/fylo-dark-theme-landing-page-master/components/top.jsx
+++ src/fylo-dark-theme-landing-page-master/components/top.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap-icons/font/bootstrap-icons.css"
-import "/Users/User/Documents/Completed Side Projects/React/my-app/src/App.css";
+import "/Users/User/Documents/Completed Side Projects/React/react-app/src/App.css";
 import illustration  from "../images/illustration-intro.png"
 import pattern  from "../images/bg-curvy-desktop.svg";
 import pattern2 from "../images/bg-curvy-mobile.svg";
